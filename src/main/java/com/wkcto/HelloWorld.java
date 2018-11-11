0b23edcd2e828b85a14644603122be7e7df99e5e
bj <bj@126.com>
2018-11-11 16:30:07 +0800

    first commit

---
+++ src/main/java/com/wkcto/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.wkcto;
+
+/**
+ * ClassName : Administrator
+ * Package : com.wkcto
+ * Description :
+ *
+ * @Data : 2018/11/11 16:23
+ * Author :xiaoxianyao@bjpowernode.com
+ */
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+    }
+
+}
