0e579e8be08879c2c61649ae16a22b6b3d60f06d
bj <bj@126.com>
2018-11-11 19:13:46 +0800

    append message

--- src/main/java/com/wkcto/HelloWorld.java
+++ src/main/java/com/wkcto/HelloWorld.java
@@ -15,6 +15,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("一切都会水到渠成的");
         System.out.println("既然上天选择了你，就会为了做好一切");
+        System.out.println("你可以收到吗");
+
     }
 
 }
