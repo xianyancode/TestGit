0bfd7426ca37c21382f9d466bf68a7e5d6938f18
bj <bj@126.com>
2018-11-11 17:26:39 +0800

    append message

---
+++ src/main/java/com/wkcto/Test1.java
@@ -0,0 +1,17 @@
+package com.wkcto;
+
+/**
+ * ClassName : Administrator
+ * Package : com.wkcto
+ * Description :
+ *
+ * @Data : 2018/11/11 17:25
+ * Author :xiaoxianyao@bjpowernode.com
+ */
+
+
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
