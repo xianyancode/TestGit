9db70e9793f67e8588d0603a6a05d0c222682a9d
bj <bj@126.com>
2018-11-11 17:33:09 +0800

    append message

--- src/main/java/com/wkcto/Test1.java
+++ src/main/java/com/wkcto/Test1.java
@@ -10,8 +10,8 @@ package com.wkcto;
  */
 
 
-public class Test1 {
+public class Test1 {//蓝色是修改过的项目，红色是新建的项目，add后文件会变成绿色，绿色和蓝色都是待提交的项目。
     public static void main(String[] args) {
-
+        System.out.println("敲代码就是多和工具、环境、代码、算法做斗争");
     }
 }
